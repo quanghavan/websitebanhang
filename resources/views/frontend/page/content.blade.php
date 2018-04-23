c31d02f4a97432933028b6ad4c99fda843e8cce1
quanghv96 <quanghavan96@gmail.com>
2018-04-23 22:47:26 +0700

    first commit

---
+++ resources/views/frontend/page/content.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section('content')
+    <style>
+        .outer-wrapper {
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: center;
+        }
+
+        .outer-wrapper > .right {
+            margin-left: 40px;
+        }
+    </style>
+    
+    <div class="outer-wrapper">
+        <div class="left">
+            @include('frontend.page.dashboard')
+        </div>
+    </div>
+@endsection
